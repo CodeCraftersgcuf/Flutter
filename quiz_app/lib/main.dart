ebae497eca4c18aa1f99d352769cbec1d19bcc2f
Abubakar Sajjad <abubakarsajjad.202202655@gcuf.edu.pk>
2024-09-28 01:13:55 +0500

    Added the Second Screen.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/start_screen.dart';
+import 'package:first_app/quiz.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: StartScreen(),
-      ),
-    ),
+    const Quiz(), // Removed the extra semicolon here
   );
 }
--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget{
-const StartScreen({super.key});
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
 
-@override
-  Widget build(context){
-    return const Text("Hello World");
+  dosomething() {
+    print("CLicked me");
+  }
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assests/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter in the Fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          OutlinedButton.icon(
+            onPressed: dosomething,
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(color: Colors.white, fontSize: 23),
+            ),
+            icon: const Icon(Icons.arrow_right_alt, color: Colors.white,),
+          )
+        ],
+      ),
+    );
   }
 }
